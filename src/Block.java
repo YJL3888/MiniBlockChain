45e6e08126e32d6e820b85d14525921bd71b017e
Yuchen Jason Liu <yj7liu@uwaterloo.ca>
2023-01-12 17:59:20 -0500

    Implemented a blockchain with manually added blocks

--- src/Block.java
+++ src/Block.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Block {
+import java.util.Arrays;
+
+public class Block {
+
+    private int previousHash;
+    private String data;
+    private int hash;
+
+    public Block(String data,int previousHash){
+
+        this.data=data;
+        this.previousHash=previousHash;
+
+        // creating the blockchain
+
+        this.hash = Arrays.hashCode(new Integer[]{data.hashCode(), previousHash});
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Block> blockChainList=new ArrayList<>();
+
+        Block genesis =new Block("Jason BlockChain", 0);
+
+        blockChainList.add(genesis);
+
+
+        Block blockX = new Block("Hello",blockChainList.get(blockChainList.size()-1).hashCode());
+        blockChainList.add(blockX);
+
+
+
+        Block blockY = new Block("This is",blockChainList.get(blockChainList.size()-1).hashCode());
+        blockChainList.add(blockX);
+
+
+        Block blockZ = new Block("My BlockChain",blockChainList.get(blockChainList.size()-1).hashCode());
+        blockChainList.add(blockX);
+
+
+        blockChainList.forEach(System.out::println);
+
+
     }
 }
+
+
+
+
+
+
+
+
